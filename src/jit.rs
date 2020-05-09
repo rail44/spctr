5dcf5433b1ad5f81ee0a6285222f9f2851b3b611
Satoshi Amemiya <amemiya@protonmail.com>
2020-05-09 18:27:07 +0900

    wip

--- src/jit.rs
+++ src/jit.rs
@@ -1,6 +1,7 @@
 use crate::token::*;
 use cranelift::prelude::*;
 use cranelift_codegen::binemit::NullTrapSink;
+use cranelift_codegen::Context;
 use cranelift_codegen::ir::types::*;
 use cranelift_module::{FuncId, Linkage, Module};
 use cranelift_simplejit::{SimpleJITBackend, SimpleJITBuilder};
@@ -9,45 +10,64 @@ use std::collections::HashMap;
 pub fn compile(ast: &AST) -> *const u8 {
     let jit_builder = SimpleJITBuilder::new(cranelift_module::default_libcall_names());
     let mut module: Module<SimpleJITBackend> = Module::new(jit_builder);
-    let mut ctx = module.make_context();
-    ctx.func.signature.returns.push(AbiParam::new(F64));
-    let mut builder_context = FunctionBuilderContext::new();
 
-    let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_context);
-    let block = builder.create_block();
-    builder.append_block_params_for_function_params(block);
-    builder.switch_to_block(block);
-    builder.seal_block(block);
+    let mut scope_ctx = ScopeContext::new(module.make_context());
 
-    let mut binds = HashMap::new();
-    let mut translator = Translator::new(&mut builder, &mut binds, &mut module);
+    let binds = HashMap::new();
+    let mut translator = Translator::new(scope_ctx.get_builder(), binds, &mut module);
     let ret = translator.translate(&ast);
 
     translator.builder.ins().return_(&[ret]);
     translator.builder.finalize();
 
     let id = module
-        .declare_function("top", Linkage::Export, &ctx.func.signature)
+        .declare_function("top", Linkage::Export, &scope_ctx.ctx.func.signature)
         .map_err(|e| e.to_string())
         .unwrap();
     module
-        .define_function(id, &mut ctx, &mut NullTrapSink {})
+        .define_function(id, &mut scope_ctx.ctx, &mut NullTrapSink {})
         .unwrap();
-    module.clear_context(&mut ctx);
+    module.clear_context(&mut scope_ctx.ctx);
     module.finalize_definitions();
     module.get_finalized_function(id)
 }
 
+struct ScopeContext {
+    pub ctx: Context,
+    builder_context: FunctionBuilderContext,
+}
+
+impl ScopeContext {
+    fn new(ctx: Context) -> ScopeContext {
+        let builder_context = FunctionBuilderContext::new();
+        ScopeContext {
+            ctx,
+            builder_context
+        }
+    }
+
+    fn get_builder<'a>(&'a mut self) -> FunctionBuilder<'a> {
+        self.ctx.func.signature.returns.push(AbiParam::new(F64));
+
+        let mut builder = FunctionBuilder::new(&mut self.ctx.func, &mut self.builder_context);
+        let block = builder.create_block();
+        builder.append_block_params_for_function_params(block);
+        builder.switch_to_block(block);
+        builder.seal_block(block);
+        builder
+    }
+}
+
 struct Translator<'a> {
-    binds: &'a mut HashMap<String, FuncId>,
-    builder: &'a mut FunctionBuilder<'a>,
+    binds: HashMap<String, FuncId>,
+    builder: FunctionBuilder<'a>,
     module: &'a mut Module<SimpleJITBackend>,
 }
 
 impl<'a> Translator<'a> {
     fn new(
-        builder: &'a mut FunctionBuilder<'a>,
-        binds: &'a mut HashMap<String, FuncId>,
+        builder: FunctionBuilder<'a>,
+        binds: HashMap<String, FuncId>,
         module: &'a mut Module<SimpleJITBackend>,
     ) -> Translator<'a> {
         Translator {
@@ -55,6 +75,10 @@ impl<'a> Translator<'a> {
             builder,
             module,
         }
+    }
+
+    fn fork(&mut self, ctx: &'a mut ScopeContext) -> Translator {
+        Translator::new(ctx.get_builder(), self.binds.clone(), self.module)
     }
 
     fn translate(&mut self, v: &Statement) -> Value {
@@ -85,7 +109,7 @@ impl<'a> Translator<'a> {
             child_builder.seal_block(block);
 
             let mut translator =
-                Translator::new(&mut child_builder, &mut self.binds, &mut self.module);
+                Translator::new(child_builder, self.binds.clone(), &mut self.module);
             let ret = translator.translate_additive(&body);
             translator.builder.ins().return_(&[ret]);
             translator.builder.finalize();
@@ -128,15 +152,15 @@ impl<'a> Translator<'a> {
                 }
             }
         }
-        return lhs;
+        lhs
     }
 
     fn translate_primary(&mut self, v: &Primary) -> Value {
         match v {
             Primary::Number(v) => self.builder.ins().f64const(v.clone()),
             Primary::Identifier(name) => {
-                let id = self.binds.get(name).unwrap().clone();
-                let func = self.module.declare_func_in_func(id, &mut self.builder.func);
+                let id = self.binds.get(name).unwrap();
+                let func = self.module.declare_func_in_func(*id, &mut self.builder.func);
                 let call = self.builder.ins().call(func, &[]);
                 self.builder.inst_results(call)[0]
             }
