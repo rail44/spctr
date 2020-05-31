4ea7ffe145d3476fba824f7a6a40002b3082ecbd
Satoshi Amemiya <amemiya@protonmail.com>
2020-06-01 01:35:27 +0900

    nits

--- src/lib/list.rs
+++ src/lib/list.rs
@@ -6,15 +6,17 @@ pub fn get_module(translator: &mut Translator) -> Vec<Cmd> {
     let mut translator = translator.fork();
     let mut block = translator.block();
 
-    block.add_bind("concat", |_| {
-        vec![Cmd::ForeignFunction(ForeignFunction(Rc::new(
-            move |_, mut args| {
-                let mut target = (*args.pop().unwrap().into_list().unwrap()).clone();
-                let mut dst = (*args.pop().unwrap().into_list().unwrap()).clone();
-                target.append(&mut dst);
-                Value::list(Rc::new(target))
-            },
-        )))]
-    });
+    block.add_bind("concat", concat);
     block.finalize()
+}
+
+fn concat(_: &mut Translator) -> Vec<Cmd> {
+    vec![Cmd::ForeignFunction(ForeignFunction(Rc::new(
+        move |_, mut args| {
+            let mut target = (*args.pop().unwrap().into_list().unwrap()).clone();
+            let mut dst = (*args.pop().unwrap().into_list().unwrap()).clone();
+            target.append(&mut dst);
+            Value::list(Rc::new(target))
+        },
+    )))]
 }
--- src/lib/string.rs
+++ src/lib/string.rs
@@ -6,14 +6,16 @@ pub fn get_module(translator: &mut Translator) -> Vec<Cmd> {
     let mut translator = translator.fork();
     let mut block = translator.block();
 
-    block.add_bind("concat", |_| {
-        vec![Cmd::ForeignFunction(ForeignFunction(Rc::new(
-            move |_, mut args| {
-                let target = args.pop().unwrap().into_string().unwrap();
-                let dst = args.pop().unwrap().into_string().unwrap();
-                Value::string(Rc::new(format!("{}{}", target, dst)))
-            },
-        )))]
-    });
+    block.add_bind("concat", concat);
     block.finalize()
+}
+
+fn concat(_: &mut Translator) -> Vec<Cmd> {
+    vec![Cmd::ForeignFunction(ForeignFunction(Rc::new(
+        move |_, mut args| {
+            let target = args.pop().unwrap().into_string().unwrap();
+            let dst = args.pop().unwrap().into_string().unwrap();
+            Value::string(Rc::new(format!("{}{}", target, dst)))
+        },
+    )))]
 }
